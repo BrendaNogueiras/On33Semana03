13706b0c884766c9f439b5eeab26391b9e59432a
NogueiraBrenda <brendanogueira2009@gmail.com>
2024-06-15 14:51:16 -0300

    Aula semana 03

---
+++ Exercicios.py/exercicio3.py
@@ -0,0 +1,6 @@
+nota = int(input('Digite sua nota: '))
+presenca = int(input('Digite sua presenca: '))
+if nota >= 7 or presenca >=75:
+    print('Você esta Aprovada')
+else:
+    print('Você esta Reprovada')

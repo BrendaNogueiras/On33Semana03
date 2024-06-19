339427adab5feac44905a279698e1e7ef2fa419a
NogueiraBrenda <brendanogueira2009@gmail.com>
2024-06-19 18:09:09 -0300

    Exercicios

--- Exercicios.py/exercicio4.py
+++ Exercicios.py/exercicio4.py
@@ -1,22 +1,21 @@
 def idade (entrada):
-
- if idade <= 15:
+ if entrada <= 15:
     return "Voce é menor de idade, não pode votar!"
- elif idade < 18:
+ elif entrada < 18:
     return "Você pode votar, mas não é obrigatório"
  else:     
-    return"Voce é obrigado a votar"
+    return "Voce é obrigado a votar"
      
     
-entrada = input("Digite sua idade: ")        
+entrada = int(input("Digite sua idade: "))       
 
 try:
-    entrada = float(idade)
-    resultado = (idade)
+    Idade = float(entrada)
+    resultado = idade(entrada)
     print(resultado)
     
 except:
-    print("Digite um numero: ")
+    print("Digite um numero inteiro: ")
 
       
  
